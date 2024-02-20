21afaf5a6677a1d6006b34eb393fe4a3e59d7b7a
AhsanRaza69 <ahsangamer0070@gmail.com>
2024-02-20 19:36:49 +0500

    change classes

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -7,7 +7,7 @@ const Logo = () => {
   return (
     <Link href="/">
       <div className=' outline-none object-cover '>
-        <Image alt='Logo' src="/images/logo.png" className=' w-[90px] md:auto' width={110} height={110}/>
+        <Image alt='Logo' src="/images/logo.png" className=' w-[90px] md:auto' width={105} height={110}/>
     </div>
     </Link>
   )
